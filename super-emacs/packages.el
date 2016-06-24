4bc7cc824d8fae58cc5d3007ca6f9fe32396f6f5
Zhang Li <xiaoxin88214@163.com>
2016-06-24 23:50:13 +0800

    org mode

--- super-emacs/packages.el
+++ super-emacs/packages.el
@@ -64,7 +64,155 @@
 (setq helm-split-window-in-side-p
       t)
 
+
 ; anaconda-mode
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
+; multi term
+(require 'multi-term)
+(setq multi-term-program "/bin/zsh")
+(setq system-uses-terminfo nil)
+
+
+;; org-mode
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (org-bullets-mode t)))
+
+(setq org-ellipsis "⤵")
+
+(setq org-src-fontify-natively t)
+
+(setq org-src-tab-acts-natively t)
+
+(setq org-src-window-setup 'current-window)
+
+;; export html get rid of footer
+(setq org-html-postamble nil)
+
+
+;; execute in emacs
+(setenv "PATH"
+        (concat (getenv "PATH")
+                ":" "/usr/local/bin"))
+
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+
+
+;; org-mode  export pdf
+(setq org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+(setq org-latex-pdf-process
+           '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+;; ;; add minted package
+(require 'ox-latex)
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+(setq org-latex-listings 'minted)
+
+
+;; (require 'ox-latex)
+;;     ;; Add minted to the defaults packages to include when exporting.
+;;     (add-to-list 'org-latex-packages-alist '("" "minted"))
+;;     ;; Tell the latex export to use the minted package for source
+;;     ;; code coloration.
+;;     (setq org-latex-listings 'minted)
+;;     ;; Let the exporter use the -shell-escape option to let latex
+;;     ;; execute external programs.
+;;     ;; This obviously and can be dangerous to activate!
+;;     (setq org-latex-minted-options
+;;           '(("mathescape" "true")
+;;             ("linenos" "true")
+;;             ("numbersep" "5pt")
+;;             ("frame" "lines")
+;;             ("framesep" "2mm")))
+;;     (setq org-latex-pdf-process
+;;           '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+;; Tex config
+(setq TeX-parse-self t)
+
+(setq TeX-PDF-mode t)
+
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (LaTeX-math-mode)
+            (setq TeX-master t)))
+
+
+(setq org-src-fontify-natively t)
+
+(require 'ox-md)
+(require 'ox-beamer)
+(require 'ox-twbs)
+(require 'ox-gfm)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (ruby . t)
+   (python . t)
+   (sh . t)))
+
+(setq org-confirm-babel-evaluate nil)
+
+(setq org-export-with-smart-quotes t)
+
+(setq org-log-done 'time)
+
+(defun mark-done-and-archive ()
+  "Mark the state of an org-mode item as DONE and archive it."
+  (interactive)
+  (org-todo 'done)
+  (org-archive-subtree))
+
+(define-key global-map "\C-c\C-x\C-s" 'mark-done-and-archive)
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cc" 'org-capture)
+
+
+;; dired config
+(require 'dired-x)
+(require 'dired+)
+(require 'dired-open)
+
+(setq dired-open-extensions
+      '(("pdf" . "preview")
+        ("md" . "emacs")
+        ("mkv" . "vlc")
+        ("mp4" . "vlc")
+        ("avi" . "vlc")))
+
+(setq-default dired-listing-switches "-lhv")
+
+(setq dired-clean-up-buffers-too t)
+
+(setq dired-recursive-copies 'always)
+
+(setq dired-recursive-deletes 'top)
+
+(flx-ido-mode 1)
+
+
+;; ido
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+(ido-ubiquitous)
+(flx-ido-mode 1) ; better/faster matching
+(setq ido-create-new-buffer 'always) ; don't confirm to create new buffers
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+;; smex
+(smex-initialize)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
