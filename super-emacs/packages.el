250724fe719aa80366589b22a7b6954ec7e0502b
Zhang Li <xiaoxin88214@163.com>
2016-09-02 14:37:30 +0800

    markdown

--- super-emacs/packages.el
+++ super-emacs/packages.el
@@ -564,3 +564,12 @@
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
+
+;; markdown mode
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
