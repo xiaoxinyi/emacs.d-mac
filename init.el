7e0f4977f64ccb478d5790bdcf16d7dfa30efcfb
Zhang Li <xiaoxin88214@163.com>
2016-11-03 12:12:45 +0800

    init.el file delete bookmark

--- init.el
+++ init.el
@@ -6,3 +6,16 @@
 
 
 (setq org-agenda-files (list "~/Desktop/org-mode/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(flymake-google-cpplint-command "/usr/local/bin/cpplint"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
