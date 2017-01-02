813b7709d05013bba808c8ef59b8abf799dcda72
Zhang Li <xiaoxin88214@163.com>
2017-01-02 13:59:52 +0800

    add snippet. ede-project.el modify.

--- ede-projects.el
+++ ede-projects.el
@@ -1,4 +1,5 @@
-;; EDE project cache file.
-;; This contains a list of projects you have visited.
-(
-)
+(ede-cpp-root-project "deepar"
+                      :file "~/Desktop/DeepAR_Algorithm/Makefile"
+                      :include-path '("/include")
+                      :system-include-path '("/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/")
+                      )
